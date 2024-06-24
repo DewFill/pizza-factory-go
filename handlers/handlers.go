30c222b87c141a7bcb4cef9c31da7c3bb13e2ea1
Artemy <24319994+DewFill@users.noreply.github.com>
2024-06-24 15:25:27 +0200

    Update to custom errors

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,29 +6,32 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/lib/pq"
 	"io"
 	"log"
 	"net/http"
+	"pizza-factory-go/custom_errors"
 	"pizza-factory-go/response"
 	"pizza-factory-go/sqlc"
+	"strconv"
 )
 
-func HandleGetOrder(ctx context.Context, queries *sqlc.Queries) http.Handler {
+// HandlerGetOrder handles the GET request to retrieve an order by its ID
+func HandlerGetOrder(ctx context.Context, queries *sqlc.Queries) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		orderId := request.PathValue("order_id")
 		order, err := queries.GetOrderWithItemIds(ctx, orderId)
 		if err != nil {
-			log.Println(err)
-			response.WriteJSON(writer, "Order not found", 404)
+			response.WritePlainText(writer, "order not found", http.StatusNotFound)
 			return
 		}
 
-		log.Println(orderId, order)
-		response.WriteJSON(writer, order, 200)
+		response.WriteJSON(writer, order, http.StatusOK)
 	})
 }
 
-func HandleCreateOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries) http.Handler {
+// HandlerCreateOrder handles the POST request to create a new order
+func HandlerCreateOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		type RequestData struct {
 			ItemIds []int32 `json:"item_ids"`
@@ -41,14 +44,19 @@ func HandleCreateOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries) h
 		decoder.DisallowUnknownFields()
 		err := decoder.Decode(&data)
 		if err != nil {
-			response.WriteJSON(writer, "Error decoding JSON", http.StatusBadRequest)
+			response.WritePlainText(writer, "error decoding JSON", http.StatusBadRequest)
 			return
 		}
 
 		// create order
 		order, err := createOrder(ctx, db, queries, data.ItemIds, false)
 		if err != nil {
+			if errors.As(err, &custom_errors.ErrIdDoesNotExist) {
+				response.WritePlainText(writer, err.Error(), http.StatusNotFound)
+				return
+			}
 			log.Printf("Error creating order: %v", err)
+			response.WriteServerError(writer)
 			return
 		}
 
@@ -62,6 +70,7 @@ type OrderWithItemIds struct {
 	IsDone  bool    `json:"done"`
 }
 
+// createOrder creates a new order with the given item IDs and done status
 func createOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries, itemIds []int32, isDone bool) (OrderWithItemIds, error) {
 	tx, err := db.Begin()
 	if err != nil {
@@ -73,11 +82,18 @@ func createOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries, itemIds
 	qtx := queries.WithTx(tx)
 	order, _ := qtx.CreateOrder(ctx, isDone)
 
+	var pqErr *pq.Error
 	var sliceItemIds []int32
 	for _, id := range itemIds {
 		var itemId int32
 		itemId, err = qtx.CreateOrderItems(ctx, sqlc.CreateOrderItemsParams{OrderID: order.ID, ItemID: id})
 		if err != nil {
+			if errors.As(err, &pqErr) {
+				if pqErr.Code == "23503" {
+					return OrderWithItemIds{}, &custom_errors.IdDoesNotExistError{Entity: "item", Id: strconv.Itoa(int(id))}
+				}
+			}
+
 			return OrderWithItemIds{}, err
 		}
 		sliceItemIds = append(sliceItemIds, itemId)
@@ -90,14 +106,15 @@ func createOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries, itemIds
 	return OrderWithItemIds{Id: order.ID, ItemIds: sliceItemIds, IsDone: order.IsDone}, nil
 }
 
-func HandleAddItemsToOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries) http.Handler {
+// HandlerAddItemsToOrder handles the POST request to add items to an existing order
+func HandlerAddItemsToOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		// Читаем тело запроса
 		orderId := request.PathValue("order_id")
 
 		body, err := io.ReadAll(request.Body)
 		if err != nil {
-			http.Error(writer, "Failed to read request body", http.StatusInternalServerError)
+			response.WritePlainText(writer, "Failed to read request body", http.StatusInternalServerError)
 			return
 		}
 		defer request.Body.Close()
@@ -108,20 +125,28 @@ func HandleAddItemsToOrder(ctx context.Context, db *sql.DB, queries *sqlc.Querie
 		// Декодируем JSON
 		err = json.Unmarshal(body, &itemIds)
 		if err != nil {
-			http.Error(writer, "Failed to decode JSON", http.StatusBadRequest)
+			response.WritePlainText(writer, "Failed to decode JSON", http.StatusBadRequest)
 			return
 		}
 
+		// Добавляем Items в Order
 		err = addItemsToOrder(ctx, db, queries, orderId, itemIds)
 		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			if errors.As(err, &custom_errors.ErrIdDoesNotExist) {
+				response.WritePlainText(writer, err.Error(), http.StatusNotFound)
+			} else if errors.As(err, &custom_errors.ErrOrderAlreadyDone) {
+				response.WritePlainText(writer, err.Error(), http.StatusConflict)
+			} else {
+				response.WritePlainText(writer, err.Error(), http.StatusInternalServerError)
+			}
 			return
 		}
 
-		writer.Write([]byte("Added items to order"))
+		response.WriteOK(writer)
 	})
 }
 
+// addItemsToOrder adds items to an existing order
 func addItemsToOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries, orderId string, itemIds []int32) error {
 	tx, err := db.Begin()
 	if err != nil {
@@ -133,16 +158,23 @@ func addItemsToOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries, ord
 	qtx := queries.WithTx(tx)
 	order, err := qtx.GetOrder(ctx, orderId)
 	if err != nil {
-		return errors.New("order not found")
+		return &custom_errors.IdDoesNotExistError{Entity: "order", Id: orderId}
 	}
 
 	if order.Done == true {
-		return errors.New("could not add new items to already done order")
+		return &custom_errors.OrderAlreadyDone{Id: orderId}
 	}
 
 	for _, id := range itemIds {
 		_, err = qtx.CreateOrderItems(ctx, sqlc.CreateOrderItemsParams{OrderID: order.OrderID, ItemID: id})
+		var pqErr *pq.Error
 		if err != nil {
+			if errors.As(err, &pqErr) {
+				if pqErr.Code == "23503" {
+					return &custom_errors.IdDoesNotExistError{Entity: "item", Id: strconv.Itoa(int(id))}
+				}
+			}
+
 			return errors.New(fmt.Sprintf("could not add item with ID %d to order", id))
 		}
 	}
@@ -154,67 +186,81 @@ func addItemsToOrder(ctx context.Context, db *sql.DB, queries *sqlc.Queries, ord
 	return nil
 }
 
+// HandlerMakeOrderDone handles the POST request to mark an order as done
 func HandlerMakeOrderDone(ctx context.Context, db *sql.DB, queries *sqlc.Queries) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		orderId := request.PathValue("order_id")
 		tx, err := db.Begin()
 		defer tx.Rollback()
+
 		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		qtx := queries.WithTx(tx)
-		status, err := qtx.GetOrderStatus(ctx, orderId)
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			log.Printf("Error setting order done: %v", err)
+			response.WriteServerError(writer)
 			return
 		}
 
-		if status == true {
-			http.Error(writer, "order is already done", http.StatusInternalServerError)
-			log.Println("order is already done")
+		qtx := queries.WithTx(tx)
+		isDone, err := qtx.GetOrderStatus(ctx, orderId)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				response.WritePlainText(writer, "order not found", http.StatusNotFound)
+				return
+			}
+
+			response.WriteServerError(writer)
+			return
+		}
+
+		if isDone == true {
+			response.WritePlainText(writer, "order is already done", http.StatusConflict)
 			return
 		}
 
 		err = qtx.SetOrderDone(ctx, orderId)
 		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			log.Printf("Error setting order done: %v", err)
+			response.WriteServerError(writer)
 			return
 		}
 
-		writer.Write([]byte("Order is done"))
-		tx.Commit()
+		err = tx.Commit()
+		if err != nil {
+			response.WriteServerError(writer)
+			return
+		}
+
+		response.WriteOK(writer)
 	})
 }
 
-func HandlerListOrders(ctx context.Context, db *sql.DB, queries *sqlc.Queries) http.Handler {
+// HandlerListOrders handles the GET request to list orders
+func HandlerListOrders(ctx context.Context, queries *sqlc.Queries) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		doneQuery := request.URL.Query().Get("done")
 
+		// list all orders without filter
 		if doneQuery == "" {
 			orders, err := queries.ListOrders(ctx)
 			if err != nil {
-				response.WriteJSON(writer, err, 200)
+				response.WriteServerError(writer)
 				return
 			}
 
 			response.WriteJSON(writer, orders, 200)
 			return
 		} else if doneQuery == "1" {
+			// list all done orders
 			orders, err := queries.ListOrdersByDone(ctx, true)
 			if err != nil {
-				response.WriteJSON(writer, err, 200)
+				response.WriteServerError(writer)
 				return
 			}
 
 			response.WriteJSON(writer, orders, 200)
 			return
 		} else if doneQuery == "0" {
+			// list all not done orders
 			orders, err := queries.ListOrdersByDone(ctx, false)
 			if err != nil {
-				response.WriteJSON(writer, err, 200)
+				response.WriteServerError(writer)
 				return
 			}
 
@@ -222,6 +268,6 @@ func HandlerListOrders(ctx context.Context, db *sql.DB, queries *sqlc.Queries) h
 			return
 		}
 
-		response.WriteJSON(writer, "done query has invalid value", 200)
+		response.WritePlainText(writer, "done query has an invalid value", 400)
 	})
 }
