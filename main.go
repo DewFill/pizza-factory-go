7690aadc8353aded74ed83141c90899369ab31cd
Artemy <24319994+DewFill@users.noreply.github.com>
2024-06-24 15:28:46 +0200

    replace db credentials with env variables

--- main.go
+++ main.go
@@ -16,15 +16,15 @@ import (
 )
 
 func main() {
+	// Load environment variables
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file")
 	}
 
-	ctx := context.Background()
-
-	db, err := sql.Open("postgres", "postgres://postgres:password@localhost/postgres?sslmode=disable")
+	// Set up database connection
+	db, err := setupDatabase()
 	if err != nil {
-		fmt.Println("Error connecting to the database:", err)
+		log.Println("Error connecting to the database:", err)
 		return
 	}
 	defer db.Close()
@@ -40,9 +40,29 @@ func main() {
 	router.Handle("POST /orders/{order_id}/items", handlers.HandleAddItemsToOrder(ctx, db, queries))
 
 	router.Handle("POST /orders/{order_id}/done", middleware.AuthHeaderRequired(handlers.HandlerMakeOrderDone(ctx, db, queries)))
+// setupDatabase initializes the database connection using environment variables
+func setupDatabase() (*sql.DB, error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
 
 	router.Handle("GET /orders", middleware.AuthHeaderRequired(handlers.HandlerListOrders(ctx, db, queries)))
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
 
+	return db, nil
+}
 	server := http.Server{
 		Addr:    os.Getenv("APP_HOSTNAME") + ":" + os.Getenv("APP_PORT"),
 		Handler: middleware.Logging(router),
