c98ba1c96fc6d9aed9c397515f43129093f3928e
Artemy <24319994+DewFill@users.noreply.github.com>
2024-06-24 15:24:27 +0200

    Add custom functions

---
+++ custom_errors/custom_errors.go
@@ -0,0 +1,27 @@
+package custom_errors
+
+import (
+	_ "errors"
+	"fmt"
+)
+
+type IdDoesNotExistError struct {
+	Entity string
+	Id     string
+}
+
+func (e *IdDoesNotExistError) Error() string {
+	return fmt.Sprintf("%s %s does not exist", e.Entity, e.Id)
+}
+
+var ErrIdDoesNotExist *IdDoesNotExistError
+
+type OrderAlreadyDone struct {
+	Id string
+}
+
+func (e *OrderAlreadyDone) Error() string {
+	return fmt.Sprintf("order %s does not exist", e.Id)
+}
+
+var ErrOrderAlreadyDone *OrderAlreadyDone
